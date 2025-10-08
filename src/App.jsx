ebc38ce35fa219cf7d5ef826bedd45b5c270a2c6
Tomas Occhiuto <tomiocchiuto@gmail.com>
2025-10-08 20:19:19 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import NavBar from './components/NavBar';
-import ItemListContainer from './components/itemlistcontainer';
+import ItemListContainer from './components/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer';
 import { Routes, Route } from 'react-router-dom';
 
