f99b729fdb121b077eeb588d18355867f8c18e8a
Tomas Occhiuto <tomiocchiuto@gmail.com>
2025-10-08 20:23:47 -0300

    Update ItemDetailContainer.jsx

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { getProductById } from '../../data/products';
+import { getProductById } from '../../data/Products';
 import ItemDetail from './ItemDetail';
 import ClipLoader from 'react-spinners/ClipLoader';
 
